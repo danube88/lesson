c8617e7f0baea1386ebcc0ee9c7b72737af02831
danube88 <36491066+danube88@users.noreply.github.com>
2020-06-17 23:52:01 +0300

    Create index.php

---
+++ lessons_on_php_first/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="../css/style-link.css" />
+        <title>Completed Lessons on PHP</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Completed lessons on PHP</h1>
+            <div class="exit">
+                <a href="../index.php">exit</a>
+            </div>
+            <ul class="older">
+                <?php
+                    $catalog = scandir('./');
+                    //print_r($catalog);
+                    $i = 0;
+                    foreach ($catalog as $key => $value) {
+                        $info = new SplFileInfo($value);
+                        if ($value != 'index.php' && $info->getExtension() == 'php') {
+                            $i++;
+                            echo '<li>';
+                            echo '<a href="'.$value.'">Task '.$i.'</a>';
+                            echo '</li>';
+                        }
+                    }
+                ?>
+            </ul>
+        </div>
+
+    </body>
+</html>
